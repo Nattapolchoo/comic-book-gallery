b77bef4d329fd64318938a2c4a17a0de7d576b87
AGODA\nchoojirawon <nattapol.choojirawong@agoda.com>
2017-06-13 17:32:48 +0700

    Addedd detail action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,12 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comic book controller";
+        }
     }
 }
+
+
+//Controller's public method is an action method that prepare the response to the view
