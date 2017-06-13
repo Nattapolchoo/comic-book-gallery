74a9e76ca3025e96ac349dba140e627a2f2ebf6f
AGODA\nchoojirawon <nattapol.choojirawong@agoda.com>
2017-06-13 18:00:41 +0700

    Updated Detail action method to use action result types

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,12 +8,18 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from the comic book controller";
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                return Redirect("/");
+            }
+            return Content("Hello from the ocmic books controller");
+            
         }
     }
 }
 
 
 //Controller's public method is an action method that prepare the response to the view
+//Content result is an action result type  
